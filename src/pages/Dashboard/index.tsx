dba0ec027ec19a393918b49b8105c1693c25abfb
dev-viniciuss <dev.mvsc@gmail.com>
2021-06-14 15:10:33 -0300

    feat: criando componente summary

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Summary } from "./Summary"
+
+import * as Styles from './styles'
+
+export function Dashboard() {
+  return(
+    <Styles.Container>
+      <Summary />
+    </Styles.Container>
+  )
+}
