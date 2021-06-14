10acdad9346c626c3906d2bddcab36c7e665b3b5
dev-viniciuss <dev.mvsc@gmail.com>
2021-06-14 15:10:08 -0300

    feat: criando página Dashboard

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
   :root {
     --red: #E52e40;
+    --green: #33CC95;
     --blue: #5429CC;
 
     --blue-light: #6933FF;
