545c3e0e3286f604330cc8c42f02fcafb9af1b09
dev-viniciuss <dev.mvsc@gmail.com>
2021-06-17 15:37:44 -0300

    feat: adicionando dinamicamente os dados na api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import { useState, FormEvent } from 'react'
 import Modal from 'react-modal'
 
+import { api } from '../../services/api'
+
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
@@ -22,6 +24,15 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
+
+    const data = {
+      title,
+      value,
+      category,
+      type,
+    }
+
+    api.post('transaction', data)
   }
 
   return(
