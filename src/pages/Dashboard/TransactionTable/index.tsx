23e7eb9d597956627783adad5f717066fd25e73f
dev-viniciuss <dev.mvsc@gmail.com>
2021-06-21 17:22:53 -0300

    refactor: deixando o TransactionTable responsivo

--- src/pages/Dashboard/TransactionTable/index.tsx
+++ src/pages/Dashboard/TransactionTable/index.tsx
@@ -1,38 +1,74 @@
+import { useMediaQuery } from 'react-responsive'
+
 import { useTransactions } from '../../../hooks/useTransactions'
 
 import * as Styles from './styles'
 
 export function TransactionTable() {
+  const isMobile = useMediaQuery({ maxWidth: 720 })
+
   const { transactions } = useTransactions()
 
   return(
-    <Styles.Container>
-      <table>
-        <thead>
-          <tr>
-            <th>Título</th>
-            <th>Valor</th>
-            <th>Categoria</th>
-            <th>Data</th>
-          </tr>
-        </thead>
+    <>  
+      { isMobile 
+        ? 
+        <Styles.ContainerMobile>
+          <header>
+            <span>Listagem</span>
+            <p>{transactions.length} itens</p>
+          </header>
 
-        <tbody>
           {transactions.map(transaction => (
-            <tr key={transaction.id}>
-              <td>{transaction.title}</td>
-              <td className={transaction.type}>
+            <Styles.CardContainer key={transaction.id}> 
+              <span>{transaction.title}</span>
+              <strong className={transaction.type}>
                 {new Intl.NumberFormat('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL'
-                }).format(transaction.amount)}
-              </td>
-              <td>{transaction.category}</td>
-              <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}</td>
-            </tr>
+                    style: 'currency',
+                    currency: 'BRL'
+                  }).format(transaction.amount)}
+              </strong>
+
+              <footer>
+                <p>{transaction.category}</p>
+                <p>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}</p>
+              </footer>
+            </Styles.CardContainer>
           ))}
-        </tbody>
-      </table>
-    </Styles.Container>
+
+        </Styles.ContainerMobile>
+        :
+        <Styles.Container>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Valor</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td>{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {new Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL'
+                    }).format(transaction.amount)}
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Styles.Container>
+      }
+    </>
   )
 }
--- src/pages/Dashboard/TransactionTable/styles.ts
+++ src/pages/Dashboard/TransactionTable/styles.ts
@@ -15,7 +15,7 @@ export const Container = styled.div`
       line-height: 1.5rem;
     }
 
-    tbody tr{
+    tbody tr {
       background: var(--shape);
     }
 
@@ -40,4 +40,70 @@ export const Container = styled.div`
 
   }
 
+`
+export const ContainerMobile = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  gap: 1rem;
+  padding: 2rem 1.5rem;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.5rem;
+
+    span {
+      font-size: 1.65rem;
+      color: var(--text-title);
+    }
+
+    p {
+      font-size: 1.2rem;
+      color: var(--text-body);
+    }
+  }
+`
+
+export const CardContainer = styled.div`
+  background: var(--shape);
+  padding: 1.7rem 1.5rem;
+  border-radius: 0.4rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  gap: 0.8rem;
+  
+  span {
+    font-size: 1.2rem;
+    color: var(--text-title);
+  }
+
+  strong {
+    font-size: 1.35rem;
+
+    &.deposit {
+      color: var(--green);
+    }
+
+    &.withdraw {
+      color: var(--red);
+    }
+  }
+
+  footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    margin-top: 1rem;
+
+    p {
+      font-size: 1.1rem;
+      color: var(--text-body);
+    }
+  }
 `
